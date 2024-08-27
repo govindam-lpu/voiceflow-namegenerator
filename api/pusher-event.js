295415bfad5648fe8ac86776dcddf2ed255de25f
govindam-lpu <govindam.12003032@lpu.in>
2024-08-27 16:13:37 +0530

    Update V2.2

--- api/pusher-event.js
+++ api/pusher-event.js
@@ -12,20 +12,28 @@ const pusher = new Pusher({
 export default function handler(req, res) {
     try {
       if (req.method !== 'POST') {
+        console.log('Invalid method:', req.method);
         return res.status(405).json({ message: 'Method not allowed' });
       }
   
       const { message } = req.body;
   
       if (!message) {
+        console.log('No message provided in request body:', req.body);
         return res.status(400).json({ message: 'Bad request, no message provided' });
       }
   
-      pusher.trigger('my-channel', 'my-event', { message: message });
+      pusher.trigger('my-channel', 'my-event', { message: message })
+        .then(() => {
+          res.status(200).json({ message: 'Event triggered successfully' });
+        })
+        .catch((error) => {
+          console.error('Error triggering Pusher event:', error);
+          res.status(500).json({ message: 'Internal Server Error' });
+        });
   
-      res.status(200).json({ message: 'Event triggered successfully' });
     } catch (error) {
-      console.error('Error triggering Pusher event:', error);
+      console.error('Unexpected error:', error);
       res.status(500).json({ message: 'Internal Server Error' });
     }
   }
